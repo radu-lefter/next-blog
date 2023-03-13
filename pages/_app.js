52c53beebda30181fb7bbd3f30d415f859b3ca48
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2023-03-13 15:07:21 +0000

    served image from public file

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import NavBar from '../components/NavBar';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
   return (    
     <>
+    <Head>
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
       <header>
         <NavBar />
       </header>
