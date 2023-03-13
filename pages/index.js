363cdf328a2d95d2c1d7e2d92002d3f2e24b87b6
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2023-03-13 21:03:46 +0000

    generating dynamic lists of links

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { getPosts } from '../lib/posts';
 
-function HomePage() {
+export async function getStaticProps() {
+  const posts = await getPosts();
+  return {
+    props: { posts },
+  };
+}
+
+function HomePage({ posts }) {
   return (
     <>
       <Head>
@@ -10,11 +18,13 @@ function HomePage() {
       <main>
         <h1>My Blog</h1>
         <ul>
-        <li>
-            <Link href="/posts/first-post">
-              First Post
-            </Link>
-          </li>
+        {posts.map((post) => (
+            <li key={post.slug}>
+              <Link href={`/posts/${post.slug}`}>
+                {post.title}
+              </Link>
+            </li>
+          ))}
         </ul>
       </main>
     </>
