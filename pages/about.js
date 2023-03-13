1f4550c008d7754eb90ad175422abeaff5621198
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2023-03-13 14:42:45 +0000

    created custom app component

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,9 @@
-import NavBar from '../components/NavBar';
+
 
 function AboutPage() {
     console.log('[AboutPage] render');
     return (
         <>
-        <header>
-          <NavBar />
-        </header>
         <main>
           <h1>About</h1>
         </main>
