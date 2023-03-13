a072cb1906bf7fed6b3339b7ef752d272716cc94
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2023-03-13 15:31:47 +0000

    added markdown file for first post

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 function HomePage() {
   return (
@@ -9,9 +10,11 @@ function HomePage() {
       <main>
         <h1>My Blog</h1>
         <ul>
-          <li>One</li>
-          <li>Two</li>
-          <li>Three</li>
+        <li>
+            <Link href="/posts/first-post">
+              First Post
+            </Link>
+          </li>
         </ul>
       </main>
     </>
